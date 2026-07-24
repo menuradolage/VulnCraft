1208f5095e18d7c872b7ec02a8e45701c7450746
MenuraD <10899255@students.plymouth.ac.uk>
2026-07-24 14:43:29 +0530

    Add unit test and GitHub Actions CI pipeline

---
+++ test_app.py
@@ -0,0 +1,15 @@
+import unittest
+from app import app
+
+class AppTestCase(unittest.Testcase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home_page(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Hello, World!', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
